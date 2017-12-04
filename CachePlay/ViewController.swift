e21b891da6d147c8e9b439f01981e11f75a5573f
wansong <betterofsong@gmail.com>
2017-12-04 17:51:28 +0800

    Initial Commit

---
+++ CachePlay/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CachePlay
+//
+//  Created by wansong.mbp.work on 04/12/2017.
+//  Copyright © 2017 wansong.mbp.work. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
