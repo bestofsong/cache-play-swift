b8181ee47665cf3ceb75e6a76092f4e3d00e93aa
wansong <betterofsong@gmail.com>
2017-12-05 20:52:25 +0800

    feat: can add frameworks

--- add-carthage.rb
+++ add-carthage.rb
@@ -1,30 +1,73 @@
 #!/usr/bin/env ruby
 
+require 'set'
 require 'xcodeproj'
 
 #system "swift package generate-xcodeproj"
+project_file_name = ARGV[0]
+target_name = ARGV[1]
+if !project_file_name || project_file_name.length <= 0
+  Kernel.abort("缺少参数：工程文件名")
+end
+carthage_package_dir = ARGV[2]
+if !carthage_package_dir || carthage_package_dir.length <= 0
+  Kernel.abort("缺少参数：Carthage根目录")
+end
 
-project = Xcodeproj::Project.open('Dependencies.xcodeproj')
+carthage_rameworks_path_ios = "#{carthage_package_dir}/Build/iOS"
+carthage_framework_files_ios = Dir.entries(carthage_rameworks_path_ios)
+.select {|f| f.end_with?(".framework")}
+.map {|name| File.expand_path(name, carthage_rameworks_path_ios)}
+.map do |abspath|
+  prefix_len = File.expand_path('.').length + 1
+  abspath.slice(prefix_len, abspath.length - prefix_len)
+end
+
+
+project_file_path = File.expand_path(project_file_name)
+project = Xcodeproj::Project.open(project_file_path)
+
+def getGroupWithName(project, name)
+  idx = project.groups.find_index {|g| g.display_name && g.display_name.eql?(name)}
+  if idx == nil
+    return nil
+  end
+  return project.groups[idx]
+end
+
+
+the_frameworks_group = getGroupWithName(project, "Frameworks")
+if !the_frameworks_group
+  the_frameworks_group = project.new_group("Frameworks")
+end
 
 project.targets.each do |target|
-  module_map_file = "Dependencies.xcodeproj/GeneratedModuleMap/#{target.name}/module.modulemap"
-
-  target.build_configurations.each do |config|
-    config.build_settings['DEFINES_MODULE'] = 'YES'
-    config.build_settings['SDKROOT'] = 'iphoneos'
-
-    # Remove this line if you prefer to link the dependencies dynamically
-    # You will also need to embed the framework with the app bundle
-    config.build_settings['MACH_O_TYPE'] = 'staticlib'
-
-    # Set MODULEMAP_FILE for non-Swift Frameworks
-    #
-    # Module maps are correctly generated for non-Swift frameworks, but SPM
-    # doesn't put the path in the build config output from generate-xcodeproj.
-    if File.exist? module_map_file
-      config.build_settings['MODULEMAP_FILE'] = "${SRCROOT}/#{module_map_file}"
-    end
+  # module_map_file = "Dependencies.xcodeproj/GeneratedModuleMap/#{target.name}/module.modulemap"
+  if !target.display_name.eql?(target_name)
+    next
   end
+
+  frameworks_build_phases = target.frameworks_build_phases
+  build_files_obj = frameworks_build_phases.files
+  build_file_list = build_files_obj.objects
+  build_filename_set = Set.new build_file_list.map{|ref| ref.display_name}
+  carthage_framework_files_ios.each do |path|
+    if build_filename_set.include?(path)
+      next
+    end
+    # create file fileRef
+    # add to group
+    ref = the_frameworks_group.new_reference(path)
+    frameworks_build_phases.add_file_reference(ref)
+    # create build file
+    # add build file to frameworks_build_phases
+  end
+  # target.frameworks_build_phases.each do |framework_phase|
+  #   framework_phase.files.each do |build_file|
+  #     file_ref = build_file
+  #     path = file_ref.path
+  #   end
+  # end
 end
 
 project.save
