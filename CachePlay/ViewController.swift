43c473c1a8e19294e4d3b52aadf5ec5d51033dcd
wansong <betterofsong@gmail.com>
2017-12-05 14:22:06 +0800

    init swift project

--- CachePlay/ViewController.swift
+++ CachePlay/ViewController.swift
@@ -9,17 +9,31 @@
 import UIKit
 
 class ViewController: UIViewController {
+  
+  let mp4 = UIView()
+  let m3u8 = UIView()
 
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view, typically from a nib.
+    view.backgroundColor = .white
+
+    let offsety0 = 60;
+    mp4.frame = CGRect(x: 0, y: offsety0, width: 100, height: 100)
+    mp4.backgroundColor = .red
+    let lbmp4 = UILabel()
+    lbmp4.text = "mp4"
+    lbmp4.sizeToFit()
+    mp4.addSubview(lbmp4)
+    
+    m3u8.frame = CGRect(x: 100, y: offsety0 + 100, width: 100, height: 100)
+    m3u8.backgroundColor = .blue
+    let lbm3u8 = UILabel()
+    lbm3u8.text = "m3u8"
+    lbm3u8.sizeToFit()
+    m3u8.addSubview(lbm3u8)
+    
+    view.addSubview(mp4)
+    view.addSubview(m3u8)
   }
-
-  override func didReceiveMemoryWarning() {
-    super.didReceiveMemoryWarning()
-    // Dispose of any resources that can be recreated.
-  }
-
-
 }
 
